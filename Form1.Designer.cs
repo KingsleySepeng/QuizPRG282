b4a9da884b7f31f04d2c87df1cf57e634f4b331d
KingsleySepeng <576944@student.belgiumcampus.ca.za>
2021-10-04 17:19:34 +0200

    This is a read text forms application

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,44 @@ namespace WPFActivity
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LbList = new System.Windows.Forms.ListBox();
+            this.btReadFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LbList
+            // 
+            this.LbList.FormattingEnabled = true;
+            this.LbList.Location = new System.Drawing.Point(164, 32);
+            this.LbList.Name = "LbList";
+            this.LbList.Size = new System.Drawing.Size(277, 173);
+            this.LbList.TabIndex = 0;
+            // 
+            // btReadFile
+            // 
+            this.btReadFile.Location = new System.Drawing.Point(228, 237);
+            this.btReadFile.Name = "btReadFile";
+            this.btReadFile.Size = new System.Drawing.Size(188, 73);
+            this.btReadFile.TabIndex = 1;
+            this.btReadFile.Text = "Read File";
+            this.btReadFile.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btReadFile);
+            this.Controls.Add(this.LbList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LbList;
+        private System.Windows.Forms.Button btReadFile;
     }
 }
 
